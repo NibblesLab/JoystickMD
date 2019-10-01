ef98fad2bad7d5cd896ee9555fd391c1c503ce51
Oh!Ishi <ohishi.nobuaki@nifty.ne.jp>
2019-10-01 15:49:16 +0900

    change logic MSX to Megadrive

--- JoystickMD/JoystickMD.ino
+++ JoystickMD/JoystickMD.ino
@@ -1,16 +1,16 @@
 // Simple gamepad example that demonstraits how to read five Arduino
 // digital pins and map them to the Arduino Joystick library.
 //
-// Arduino port         MSX compatible
-//  Pin 0 (PD2)  -----     3
-//  Pin 1 (PD3)  -----     4
-//  Pin 2 (PD1)  -----     2
-//  Pin 3 (PD0)  -----     1
-//  Pin 4 (PD4)  -----     6
-//  Pin 5 (PC6)  -----     8
-//  Pin 6 (PD7)  -----     7
-//         +5V   -----     5
-//         GND   -----     9
+// Arduino port         Megadrive
+//  Pin 3 (PD0)  -----    1 (D0)
+//  Pin 2 (PD1)  -----    2 (D1)
+//  Pin 0 (PD2)  -----    3 (D2)
+//  Pin 1 (PD3)  -----    4 (D3)
+//         +5V   -----    5
+//  Pin 4 (PD4)  -----    6 (D4)
+//  Pin 5 (PC6)  -----    7
+//         GND   -----    8
+//  Pin 6 (PD7)  -----    9 (D5)
 //
 // NOTE: This sketch file is for use with Arduino Leonardo and
 //       Arduino Micro only.
@@ -51,8 +51,10 @@ void setup() {
 
 // Last state of the buttons
 int lastButtonState[4] = {1,1,1,1};
-int lastButtonState_trig_A = 0x80;
+int lastButtonState_trig_A = 0x10;
 int lastButtonState_trig_B = 0x10;
+int lastButtonState_trig_C = 0x80;
+int lastButtonState_trig_ST = 0x80;
 int lastStickState_X = 0;
 int lastStickState_Y = 0;
 
@@ -60,11 +62,47 @@ void loop() {
   int currentButtonState;
   int currentButtonState_trig_A;
   int currentButtonState_trig_B;
+  int currentButtonState_trig_C;
+  int currentButtonState_trig_ST;
   bool F_operate = false;
+  int state;
+
+  digitalWrite(5, 0);
+  delayMicroseconds(10);
+  currentButtonState = PIND;
+
+  // TRIGGER A
+  state = currentButtonState & 0x10;
+  if (state != lastButtonState_trig_A)
+  {
+    if (state == 0) {
+      Joystick.pressButton(2);
+    } else {
+      Joystick.releaseButton(2);
+    }
+    lastButtonState_trig_A = state;
+    F_operate = true;
+  }
+
+  // TRIGGER START
+  state = currentButtonState & 0x80;
+  if (state != lastButtonState_trig_ST)
+  {
+    if (state == 0) {
+      Joystick.pressButton(9);
+    } else {
+      Joystick.releaseButton(9);
+    }
+    lastButtonState_trig_ST = state;
+    F_operate = true;
+  }
+
+  digitalWrite(5, 1);
+  delayMicroseconds(10);
+  currentButtonState = PIND;
 
   // UP
-  currentButtonState = PIND;
-  int state = currentButtonState & 0x01;
+  state = currentButtonState & 0x01;
   if (state != lastButtonState[0]) {
     if (state == 0) {
       Joystick.setYAxis(0);
@@ -111,21 +149,8 @@ void loop() {
     F_operate = true;
   }
 
-  // TRIGGER A
-  state = currentButtonState & 0x10;
-  if (state != lastButtonState_trig_A)
-  {
-    if (state == 0) {
-      Joystick.pressButton(2);
-    } else {
-      Joystick.releaseButton(2);
-    }
-    lastButtonState_trig_A = state;
-    F_operate = true;
-  }
-
   // TRIGGER B
-  state = currentButtonState & 0x80;
+  state = currentButtonState & 0x10;
   if (state != lastButtonState_trig_B)
   {
     if (state == 0) {
@@ -134,6 +159,19 @@ void loop() {
       Joystick.releaseButton(1);
     }
     lastButtonState_trig_B = state;
+    F_operate = true;
+  }
+
+  // TRIGGER C
+  state = currentButtonState & 0x80;
+  if (state != lastButtonState_trig_C)
+  {
+    if (state == 0) {
+      Joystick.pressButton(4);
+    } else {
+      Joystick.releaseButton(4);
+    }
+    lastButtonState_trig_C = state;
     F_operate = true;
   }
 
